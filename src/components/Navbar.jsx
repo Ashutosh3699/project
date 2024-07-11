8a595e48be29c9be268c42cb1830223546129251
Ashutosh3699 <bishoiashutosh@gmail.com>
2024-07-11 23:13:34 +0530

    media queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React,{useState} from 'react'
+import { NavLink } from 'react-router-dom';
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+
+    const [show, setShow] = useState("hidden");
+
+    function showFunction(){
+
+        if(show === "hidden"){
+            setShow("flex");
+        }
+        else{
+            setShow("hidden");
+        }
+    }
+
   return (
-    <div className='w-full flex justify-around items-center bg-regal-blue text-white opacity-80  px-4  py-5'>
+    <div className='w-full flex justify-between lg:justify-around items-center bg-regal-blue text-white opacity-80  px-4  py-5'>
     
     <div>
     <NavLink  to={'/'}>
         <img src='' alt='image'  />
     </NavLink>
     </div>
-    <ul  className='flex gap-6 text-lg'>
+    <div  className=' flex lg:hidden relative'  onClick={showFunction}>
+        <GiHamburgerMenu />
+        <ul  className={`${show} flex-col  gap-3 absolute top-4 right-0 bg-blue-color  text-md  cursor-pointer`}>
         <li>
             <NavLink  to={"/about-us"}>
                 About-us
@@ -48,8 +64,48 @@ const Navbar = () => {
         </li>
 
     </ul>
+    </div>
 
-    <ul className='flex gap-5 '>
+    <ul  className='lg:flex gap-6 text-lg hidden  '>
+        <li className='cursor-pointer'>
+            <NavLink  to={"/about-us"}>
+                About-us
+            </NavLink>
+        </li>
+        <li>
+            <NavLink  to={"/blogs"}>
+                Blogs
+            </NavLink>
+        </li>
+        <li>
+            <NavLink  to={"/stores"}>
+                Stores
+            </NavLink>
+        </li>
+        <li>
+            <NavLink  to={"/our-field"}>
+                Our fields
+            </NavLink>
+        </li>
+        <li>
+            <NavLink  to={"/career"}>
+                Career
+            </NavLink>
+        </li>
+        <li>
+            <NavLink  to={"/partnership"}>
+                Partnership
+            </NavLink>
+        </li>
+        <li>
+            <NavLink  to={"/contact-us"}>
+                Contact Us
+            </NavLink>
+        </li>
+
+    </ul>
+
+    <ul className='lg:flex gap-5  hidden '>
     <li>
             <NavLink  to={"/login"}>
                 
