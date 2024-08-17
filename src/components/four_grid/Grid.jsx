2a8bb26a2fb446c7bd1673cc9a7b3a0eccf84151
Ashutosh3699 <bishoiashutosh@gmail.com>
2024-08-17 15:02:25 +0530

    grid change

--- src/components/four_grid/Grid.jsx
+++ src/components/four_grid/Grid.jsx
@@ -30,7 +30,7 @@ const Grid = () => {
         <div className='w-[95%] md:w-[48%] overflow-hidden relative '>
             <img src={image3}  alt='image1'/>
             <div className='absolute z-10 px-4 py-2 top-0 md:top-4 w-full flex flex-col gap-y-2 items-center  '>
-              <h1 className='text-xl md:text-4xl  text-white'>KRISHIMITRA A-10H</h1>
+              <h1 className='text-xl md:text-4xl  text-white'></h1>
               {/* <button className='py-1 px-10 text-md  text-white rounded-full  border-2 border-white  shadow-2xl
               hover:bg-white hover:bg-opacity-20  transition-all duration-300'>Buy Now  </button> */}
           </div>
@@ -40,7 +40,7 @@ const Grid = () => {
         <div className='w-[95%] md:w-[48%] overflow-hidden relative '>
             <img src={image4}  alt='image1'/>
             <div className='absolute z-10 px-4 py-2 top-0 md:top-4 w-full flex flex-col gap-y-2 items-center  '>
-              <h1 className='text-xl md:text-4xl  text-white'>KRISHIMITRA A-10H</h1>
+              <h1 className='text-xl md:text-4xl  text-white'></h1>
               {/* <button className='py-1 px-10 text-md  text-white rounded-full  border-2 border-white  shadow-2xl
               hover:bg-white hover:bg-opacity-20  transition-all duration-300'>Buy Now  </button> */}
           </div>
